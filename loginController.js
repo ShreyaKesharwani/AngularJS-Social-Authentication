57a2034a858101b770c7b469f74a1b52ef4090ec
ShreyaKesharwani <kesharwani.shubhi@gmail.com>
2017-07-12 12:50:24 +0530

    Add files via upload

---
+++ loginController.js
@@ -0,0 +1,32 @@
+(function () {
+    "use strict";
+    app.controller("loginController",loginController);
+    loginController.$inject=["$scope","loginService","$localStorage","$location"];
+    function loginController($scope,loginService,$localStorage,$location) {
+        $scope.login={};
+        $scope.login_details = function () {
+           loginService.authenicate($scope.login).then(function (response) {
+              if(response.data.login=="success"){
+                  $localStorage.slambook=response;
+                  $location.path("/slam");
+              }else{
+                  alert("Wrong Details !");
+              }
+           });
+        };
+    };
+})();
+
+
+
+
+/*
+(function () {
+    "use strict";
+    app.controller("registerController",registerController);
+    registerController.$inject=["$scope"];
+    function registerController($scope) {
+        $scope.register="Registaration Page Soon....";
+    }
+
+})();*/
